28c955c0ec878758d92e47f089a661644b6c2629
andgeral <geraldinebecerra93@gmail.com>
2024-04-22 00:32:10 -0400

    se crea ruta para css y ruta cargar

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const app = express();
+const Jimp = require("jimp");
+const uuid = require("uuid");
 
+//Ruta para el css
+app.use("/css", express.static(__dirname + "/assets/css"));
+
+//Ruta raiz para visulizar el formulario
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/cargar", async (req, res) => {
+  try {
+    const { imagen } = req.query;
+
+    const image = await Jimp.read(`./${imagen}`);
+    const cambioNombreImagen = uuid.v4().slice(0, 6);
+    await image
+      .resize(350, Jimp.AUTO)
+      .greyscale()
+      .writeAsync(`${cambioNombreImagen}.jpeg`);
+    res.sendFile(`${cambioNombreImagen}`);
+  } catch (error) {
+    console.log("error en el catch: ", error.message);
+    console.log("codigo del error en lectura de archivo: ", error.code);
+    if (error.code == "ENOENT") {
+      res.status(404).send("Archivo no encontrado");
+    } else {
+      res.status(500).send("Error al procesar la imagen");
+    }
+  }
 });
 
 app.listen(3000, () => {
